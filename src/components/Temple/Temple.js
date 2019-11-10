e752578af13cec9fe584229b17bcd08d221eaa27
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 22:15:45 -0500

    add temple img paths

--- src/components/Temple/Temple.js
+++ src/components/Temple/Temple.js
@@ -1,6 +1,11 @@
 import React from "react"
 import styled from "styled-components"
 import "./temple.css"
+import card1 from "./temple/1.png"
+import card2 from "./temple/2.png"
+import card3 from "./temple/3.png"
+import card4 from "./temple/4.png"
+import cardback from "./temple/cardback.png"
 
 const Container = styled.section`
   text-align: center;
@@ -15,34 +20,34 @@ const Temple = () => {
       <div className="card__container">
         <div className="card card__1">
           <div className="card__back">
-            <img src="temple/1.png" alt="" className="card card1" />
+            <img src={card1} alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/cardback.jpeg" alt="" />
+            <img src={cardback} alt="" />
           </div>
         </div>
         <div className="card card__2">
           <div className="card__back">
-            <img src="temple/2.png" alt="" className="card card1" />
+            <img src={card2} alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/cardback.jpeg" alt="" />
+            <img src={cardback} alt="" />
           </div>
         </div>
         <div className="card card__3">
           <div className="card__back">
-            <img src="temple/3.png" alt="" className="card card1" />
+            <img src={card3} alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/cardback.jpeg" alt="" />
+            <img src={cardback} alt="" />
           </div>
         </div>
         <div className="card card__4">
           <div className="card__back">
-            <img src="temple/4.png" alt="" className="card card1" />
+            <img src={card4} alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/cardback.jpeg" alt="" />
+            <img src={cardback} alt="" />
           </div>
         </div>
       </div>
