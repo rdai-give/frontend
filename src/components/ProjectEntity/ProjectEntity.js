bce70593e122a19cdb6c1422f0a13c1b90e41af8
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 19:24:00 -0500

    add temple assets to images

--- src/components/ProjectEntity/ProjectEntity.js
+++ src/components/ProjectEntity/ProjectEntity.js
@@ -15,7 +15,46 @@ const ProjectEntity = ({ project }) => {
       {image}
       {category}
       {twitter}
-    </Container>
+      <aside class="torch__container">
+        <img class="torch torch__1" src="temple/torch.png" alt="" />
+        <img class="torch torch__2" src="temple/torch.png" alt="" />
+      </aside>
+      <div class="card__container">
+        <div class="card card__1">
+          <div class="card__back">
+            <img src="temple/1.png" alt="" class="card card1" />
+          </div>
+          <div class="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div class="card card__2">
+          <div class="card__back">
+            <img src="temple/2.png" alt="" class="card card1" />
+          </div>
+          <div class="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div class="card card__3">
+          <div class="card__back">
+            <img src="temple/3.png" alt="" class="card card1" />
+          </div>
+          <div class="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div class="card card__4">
+          <div class="card__back">
+            <img src="temple/4.png" alt="" class="card card1" />
+          </div>
+          <div class="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+      </div>
+
+    </Container >
   )
 }
 
