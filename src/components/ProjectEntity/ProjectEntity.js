8d079e5d613f702aec853748a38513636fe5d633
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 19:56:22 -0500

    move temple component

--- src/components/ProjectEntity/ProjectEntity.js
+++ src/components/ProjectEntity/ProjectEntity.js
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 import PropTypes from "prop-types"
-import "./temple.css"
 
 const Container = styled.section`
   text-align: center;
@@ -16,46 +15,45 @@ const ProjectEntity = ({ project }) => {
       {image}
       {category}
       {twitter}
-      <aside class="torch__container">
-        <img class="torch torch__1" src="temple/torch.png" alt="" />
-        <img class="torch torch__2" src="temple/torch.png" alt="" />
+      <aside className="torch__container">
+        <img className="torch torch__1" src="temple/torch.png" alt="" />
+        <img className="torch torch__2" src="temple/torch.png" alt="" />
       </aside>
-      <div class="card__container">
-        <div class="card card__1">
-          <div class="card__back">
-            <img src="temple/1.png" alt="" class="card card1" />
+      <div className="card__container">
+        <div className="card card__1">
+          <div className="card__back">
+            <img src="temple/1.png" alt="" className="card card1" />
           </div>
-          <div class="card__front">
+          <div className="card__front">
             <img src="temple/card__back.jpeg" alt="" />
           </div>
         </div>
-        <div class="card card__2">
-          <div class="card__back">
-            <img src="temple/2.png" alt="" class="card card1" />
+        <div className="card card__2">
+          <div className="card__back">
+            <img src="temple/2.png" alt="" className="card card1" />
           </div>
-          <div class="card__front">
+          <div className="card__front">
             <img src="temple/card__back.jpeg" alt="" />
           </div>
         </div>
-        <div class="card card__3">
-          <div class="card__back">
-            <img src="temple/3.png" alt="" class="card card1" />
+        <div className="card card__3">
+          <div className="card__back">
+            <img src="temple/3.png" alt="" className="card card1" />
           </div>
-          <div class="card__front">
+          <div className="card__front">
             <img src="temple/card__back.jpeg" alt="" />
           </div>
         </div>
-        <div class="card card__4">
-          <div class="card__back">
-            <img src="temple/4.png" alt="" class="card card1" />
+        <div className="card card__4">
+          <div className="card__back">
+            <img src="temple/4.png" alt="" className="card card1" />
           </div>
-          <div class="card__front">
+          <div className="card__front">
             <img src="temple/card__back.jpeg" alt="" />
           </div>
         </div>
       </div>
-
-    </Container >
+    </Container>
   )
 }
 
