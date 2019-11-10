fbad7726ce2de5143875375609450d13a1e88bd9
Patrick Gallagher <blockchainbuddha@gmail.com>
2019-11-10 00:00:42 -0500

    fix build

--- src/pages/altar.js
+++ src/pages/altar.js
@@ -1,10 +1,10 @@
-// import React from "react"
-import React, { useContext } from "react"
+import React from "react"
+// import React, { useContext } from "react"
 
 import styled from "styled-components"
 import Layout from "../components/Layout/Layout"
-import { Context } from "../components/context"
-import ProjectCard from "../components/ProjectCard/ProjectCard"
+// import { Context } from "../components/context"
+// import ProjectCard from "../components/ProjectCard/ProjectCard"
 
 import "../components/fonts.css"
 
@@ -13,16 +13,16 @@ const Container = styled.section`
 `
 
 const Altar = () => {
-  const [context] = useContext(Context)
-  const { selectedCards } = context
+  // const [context] = useContext(Context)
+  // const { selectedCards } = context
 
   const CardList = () => {
-    let list
-    if (typeof selectedCards !== "undefined") {
-      list = selectedCards.map(project => {
-        return <ProjectCard key={`${project.name}`} project={project} />
-      })
-    }
+    const list = []
+    // if (typeof selectedCards !== "undefined") {
+    //   list = selectedCards.map(project => {
+    //     return <ProjectCard key={`${project.name}`} project={project} />
+    //   })
+    // }
     return list
   }
 
