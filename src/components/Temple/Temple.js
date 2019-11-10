ce05a29e5209eacefaa3dc20370e6c5659b846a9
Cindy Sweetpea <cindychau24@me.com>
2019-11-10 02:16:55 -0500

    put up the creepy face

--- src/components/Temple/Temple.js
+++ src/components/Temple/Temple.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react"
 import styled from "styled-components"
 import "./temple.css"
-import torch from "./temple/torch.png"
-import Card from "../Cards/Cards"
 
 const Container = styled.section`
   text-align: center;
@@ -16,14 +14,6 @@ class Temple extends Component {
   render() {
     return (
       <Container>
-        <aside className="torch__container">
-          <img className="torch torch__1" src={torch} alt="" />
-          <img className="torch torch__2" src={torch} alt="" />
-        </aside>
-        <Card />
-        <a href="/" className="share__link">
-          Place Your Offering
-        </a>
         <img src="bigg.png" alt="" className="original" />
         <img src="redeyes.png" alt="" className="eyes" />
       </Container>
--- src/pages/altar.js
+++ src/pages/altar.js
@@ -7,6 +7,9 @@ import styled from "styled-components"
 // import ProjectCard from "../components/ProjectCard/ProjectCard"
 
 import "../components/fonts.css"
+import "../components/Temple/temple.css"
+import big from "../images/bigg.png"
+import redeyes from "../images/redeyes.png"
 
 const Container = styled.section`
   text-align: center;
@@ -16,20 +19,21 @@ const Altar = () => {
   // const [context] = useContext(Context)
   // const { selectedCards } = context
 
-  const CardList = () => {
-    const list = []
-    // if (typeof selectedCards !== "undefined") {
-    //   list = selectedCards.map(project => {
-    //     return <ProjectCard key={`${project.name}`} project={project} />
-    //   })
-    // }
-    return list
-  }
+  // const CardList = () => {
+  //   const list = []
+  //   // if (typeof selectedCards !== "undefined") {
+  //   //   list = selectedCards.map(project => {
+  //   //     return <ProjectCard key={`${project.name}`} project={project} />
+  //   //   })
+  //   // }
+  //   return list
+  // }
 
   return (
     <>
       <Container>
-        <CardList />
+        <img src={big} alt="" className="original" />
+        <img src={redeyes} alt="" className="eyes" />
       </Container>
     </>
   )
