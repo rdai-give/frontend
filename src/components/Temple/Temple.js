b2b29eeff77e576ed0075ffda514b418f6b3d221
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 22:00:26 -0500

    add giger cardback up

--- src/components/Temple/Temple.js
+++ src/components/Temple/Temple.js
@@ -18,7 +18,7 @@ const Temple = () => {
             <img src="temple/1.png" alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/card__back.jpeg" alt="" />
+            <img src="images/cardback.jpeg" alt="" />
           </div>
         </div>
         <div className="card card__2">
@@ -26,7 +26,7 @@ const Temple = () => {
             <img src="temple/2.png" alt="" className="card card1" />
           </div>
           <div className="card__front">
-            <img src="temple/card__back.jpeg" alt="" />
+            <img src="images/cardback.jpeg" alt="" />
           </div>
         </div>
         <div className="card card__3">
