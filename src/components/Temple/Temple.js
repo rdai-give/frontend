4d69aede9d76b8c27c2c9c9762bf5b3247439e01
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 22:31:08 -0500

    update cardbacks

--- src/components/Temple/Temple.js
+++ src/components/Temple/Temple.js
@@ -5,7 +5,8 @@ import card1 from "./temple/1.png"
 import card2 from "./temple/2.png"
 import card3 from "./temple/3.png"
 import card4 from "./temple/4.png"
-import cardback from "./temple/cardback.png"
+import torch from "./temple/torch.png"
+import cardback from "./temple/cardb.png"
 
 const Container = styled.section`
   text-align: center;
@@ -14,8 +15,8 @@ const Temple = () => {
   return (
     <Container>
       <aside className="torch__container">
-        <img className="torch torch__1" src="temple/torch.png" alt="" />
-        <img className="torch torch__2" src="temple/torch.png" alt="" />
+        <img className="torch torch__1" src={torch} alt="" />
+        <img className="torch torch__2" src={torch} alt="" />
       </aside>
       <div className="card__container">
         <div className="card card__1">
@@ -50,6 +51,10 @@ const Temple = () => {
             <img src={cardback} alt="" />
           </div>
         </div>
+      </div>
+      <div>
+        <a href="#" className="share__link">Place Your Offering</a>
+        <a href="#" className="back__link">back</a>
       </div>
     </Container>
   )
