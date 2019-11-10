2f51648d94a1401c78c067c26f0dbc4e55e78c6f
Cindy Sweetpea <cindychau24@me.com>
2019-11-09 20:34:29 -0500

    change temple.js

--- src/components/Temple/Temple.js
+++ src/components/Temple/Temple.js
@@ -5,48 +5,48 @@ const Container = styled.section`
   text-align: center;
 `
 const Temple = () => {
-    return (
-        <Container>
-            <aside className="torch__container">
-                <img className="torch torch__1" src="temple/torch.png" alt="" />
-                <img className="torch torch__2" src="temple/torch.png" alt="" />
-            </aside>
-            <div className="card__container">
-                <div className="card card__1">
-                    <div className="card__back">
-                        <img src="temple/1.png" alt="" className="card card1" />
-                    </div>
-                    <div className="card__front">
-                        <img src="temple/card__back.jpeg" alt="" />
-                    </div>
-                </div>
-                <div className="card card__2">
-                    <div className="card__back">
-                        <img src="temple/2.png" alt="" className="card card1" />
-                    </div>
-                    <div className="card__front">
-                        <img src="temple/card__back.jpeg" alt="" />
-                    </div>
-                </div>
-                <div className="card card__3">
-                    <div className="card__back">
-                        <img src="temple/3.png" alt="" className="card card1" />
-                    </div>
-                    <div className="card__front">
-                        <img src="temple/card__back.jpeg" alt="" />
-                    </div>
-                </div>
-                <div className="card card__4">
-                    <div className="card__back">
-                        <img src="temple/4.png" alt="" className="card card1" />
-                    </div>
-                    <div className="card__front">
-                        <img src="temple/card__back.jpeg" alt="" />
-                    </div>
-                </div>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <aside className="torch__container">
+        <img className="torch torch__1" src="temple/torch.png" alt="" />
+        <img className="torch torch__2" src="temple/torch.png" alt="" />
+      </aside>
+      <div className="card__container">
+        <div className="card card__1">
+          <div className="card__back">
+            <img src="temple/1.png" alt="" className="card card1" />
+          </div>
+          <div className="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="card card__2">
+          <div className="card__back">
+            <img src="temple/2.png" alt="" className="card card1" />
+          </div>
+          <div className="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="card card__3">
+          <div className="card__back">
+            <img src="temple/3.png" alt="" className="card card1" />
+          </div>
+          <div className="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+        <div className="card card__4">
+          <div className="card__back">
+            <img src="temple/4.png" alt="" className="card card1" />
+          </div>
+          <div className="card__front">
+            <img src="temple/card__back.jpeg" alt="" />
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
 }
 
 export default Temple
